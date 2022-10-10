142b826b542a019262e8a19b5b46c06a7bf7f3b1
Abidur Rashid <mras0018>
2022-10-10 22:52:42 +0800

    added more dataset for visualisation 2

---
+++ visualisation2/choropleth_map.js
@@ -0,0 +1,6 @@
+var vg_1 = "json_files/choropleth_map.json";
+vegaEmbed("#choropleth_map", vg_1).then(function (result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
---
+++ visualisation2/vega_comp.js
@@ -0,0 +1,14 @@
+var vg_1 = "json_files/choropleth_map.json";
+vegaEmbed("#choropleth_map", vg_1).then(function (result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
+
+var vg_2 = "json_files/line_chart.json";
+vegaEmbed("#line_chart", vg_2).then(function (result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
+
