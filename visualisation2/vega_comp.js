f68a0faa26cbc3c5f9b86a0876c178774d4c84b7
Abidur Rashid <mras0018>
2022-10-11 23:58:35 +0800

    4 visualisations complete

--- visualisation2/vega_comp.js
+++ visualisation2/vega_comp.js
@@ -12,3 +12,17 @@ vegaEmbed("#line_chart", vg_2).then(function (result) {
     
 }).catch(console.error);
 
+var vg_3 = "json_files/donut.json";
+vegaEmbed("#donut", vg_3).then(function (result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
+
+var vg_4 = "json_files/heatmap.json";
+vegaEmbed("#heatmap", vg_4).then(function (result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    
+}).catch(console.error);
+
